2d5eec519cc7608e574c250d23dc955811c3a9d3
rodnune <rpaul_94@hotmail.com>
2017-07-24 20:17:41 +0200

    Anadida validacion para enviar email

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -7,11 +7,27 @@ use Illuminate\Http\Request;
 use Validator;
 use Session;
 
+define('to_address','jadiego@infor.uva.es');
+
 class MailController extends \App\Http\Controllers\Controller
 {
 
+
+
     public function enviarMensaje(Request $request){
-        $to_address = 'jadiego@infor.uva.es';
+
+
+        $validator = Validator::make($request->all(), [
+
+            'from_name'  => 'required|string',
+            'from_email' => 'required|email',
+            'message'    => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/contactar')
+                ->withErrors($validator);
+        }
 
         $from_name = $request->input('from_name');
         $from_address = $request->input('from_email');
@@ -23,17 +39,17 @@ class MailController extends \App\Http\Controllers\Controller
         $message = 'Mensaje enviado por: ' . $from_name . ' (' . $from_address . ')' .
             'Enviado el: ' . date("l d/M/Y H:i:s O")  . $message;
 
-        $success = mail($to_address, $subject, $message, $headers);
+        $success = mail(to_address, $subject, $message, $headers);
+
+
+
 
 
         if($success){
             return redirect('/contactar')->with(['success' => $message, 'name' => $from_name, 'email' => $from_address,
             'subject' => $subject]);
-        }else{
-            return redirect('/contactar');
         }
 
-        return redirect('/contactar');
     }
 
 }
